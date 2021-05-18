895d4d769b67517e13aa28a020cc61aec6a172ca
kenny <kenny397@naver.com>
2021-05-18 17:32:08 +0900

    요구사항3- Post방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,6 +9,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -16,64 +17,81 @@ import org.slf4j.LoggerFactory;
 
 import model.User;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
-    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+	private Socket connection;
 
-    public RequestHandler(Socket connectionSocket) {
-        this.connection = connectionSocket;
-    }
+	public RequestHandler(Socket connectionSocket) {
+		this.connection = connectionSocket;
+	}
 
-    public void run() {
-        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-        	
-        	BufferedReader br=new BufferedReader(new InputStreamReader(in,"UTF-8"));
-        	String line=br.readLine();
-        	String url=HttpRequestUtils.getUrl(line);
-        	if(url.startsWith("/user/create")) {
-        		int index=url.indexOf("?");
-        		String queryString = url.substring(index+1);
-        		Map<String,String> params= HttpRequestUtils.parseQueryString(queryString);
-        		User user= new User(params.get("userId"),params.get("pasword"),params.get("name"),params.get("email"));
-        		log.debug("User : {}", user);
-        		url="/index.html";
-        	}
+	public void run() {
+		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
+				connection.getPort());
+		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+
+			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+			String line = br.readLine();
+			String url = HttpRequestUtils.getUrl(line);
+
+			log.debug("request line : {}", line);
+			if (line == null)
+				return;
+			Map<String,String> headers=new HashMap<String,String>();
+			
+			while (!line.equals("")) {
+				log.debug("header : {}", line);
+				line = br.readLine();
+				String[]headerTokens=line.split(": ");
+				if(headerTokens.length==2) {
+					headers.put(headerTokens[0], headerTokens[1]);
+				}
+			}
+			log.debug("Content-Length : {}",headers.get("Content-Length"));
+			if (url.startsWith("/user/create")) {
+				String requestBody=IOUtils.readData(br,Integer.parseInt(headers.get("Content-Length")));
+				log.debug("Request Body : {}",requestBody);
+				Map<String, String> params = HttpRequestUtils.parseQueryString(requestBody);
+				User user = new User(params.get("userId"), params.get("pasword"), params.get("name"),
+						params.get("email"));
+				log.debug("User : {}", user);
+				url = "/index.html";
+			}
 			/*
-			 * log.debug("request line : {}",line); if(line==null)return; String[]
+			 * 요구사항 1번 log.debug("request line : {}",line); if(line==null)return; String[]
 			 * tokens=line.split(" "); while(!line.equals("")) { line=br.readLine();
 			 * log.debug("header : {}",line); }
 			 */
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
+			DataOutputStream dos = new DataOutputStream(out);
+			byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+			response200Header(dos, body.length);
+			responseBody(dos, body);
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
+	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+		try {
+			dos.writeBytes("HTTP/1.1 200 OK \r\n");
+			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
+	private void responseBody(DataOutputStream dos, byte[] body) {
+		try {
+			dos.write(body, 0, body.length);
+			dos.flush();
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
 }
