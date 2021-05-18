442c01a9f7a2c8831ac6703eb3ca3d2026542b59
kenny <kenny397@naver.com>
2021-05-18 16:49:17 +0900

    get방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,13 +1,21 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -21,11 +29,27 @@ public class RequestHandler extends Thread {
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
-
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	
+        	BufferedReader br=new BufferedReader(new InputStreamReader(in,"UTF-8"));
+        	String line=br.readLine();
+        	String url=HttpRequestUtils.getUrl(line);
+        	if(url.startsWith("/user/create")) {
+        		int index=url.indexOf("?");
+        		String queryString = url.substring(index+1);
+        		Map<String,String> params= HttpRequestUtils.parseQueryString(queryString);
+        		User user= new User(params.get("userId"),params.get("pasword"),params.get("name"),params.get("email"));
+        		log.debug("User : {}", user);
+        		url="/index.html";
+        	}
+			/*
+			 * log.debug("request line : {}",line); if(line==null)return; String[]
+			 * tokens=line.split(" "); while(!line.equals("")) { line=br.readLine();
+			 * log.debug("header : {}",line); }
+			 */
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
