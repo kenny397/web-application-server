c40550a8e3451ef844c0fd2ed622df71c4b0d518
kenny <kenny397@naver.com>
2021-05-24 12:00:18 +0900

    쿠키사용

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -15,6 +15,7 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import db.DataBase;
 import model.User;
 import util.HttpRequestUtils;
 import util.IOUtils;
@@ -56,12 +57,39 @@ public class RequestHandler extends Thread {
 				String requestBody=IOUtils.readData(br,Integer.parseInt(headers.get("Content-Length")));
 				log.debug("Request Body : {}",requestBody);
 				Map<String, String> params = HttpRequestUtils.parseQueryString(requestBody);
-				User user = new User(params.get("userId"), params.get("pasword"), params.get("name"),
+				User user = new User(params.get("userId"), params.get("password"), params.get("name"),
 						params.get("email"));
 				log.debug("User : {}", user);
+				DataBase.addUser(user);
+				DataOutputStream dos = new DataOutputStream(out);
+				response302Header(dos);
+			}else if(url.equals("/user/login")) {
+				String requestBody=IOUtils.readData(br,Integer.parseInt(headers.get("Content-Length")));
+				log.debug("Request Body : {}",requestBody);
+				Map<String, String> params = HttpRequestUtils.parseQueryString(requestBody);
+				User user=DataBase.findUserById(params.get("userId"));
+				if(user==null) {
+					log.debug("User Not Found");
+					DataOutputStream dos = new DataOutputStream(out);
+					response302Header(dos);
+				}else if(user.getPassword().equals(params.get("password"))) {
+					log.debug("login success");
+					DataOutputStream dos = new DataOutputStream(out);
+					response302HeaderWithCookie(dos,"logined=true");
+				}else {
+					log.debug("Password Mismatch");
+					DataOutputStream dos = new DataOutputStream(out);
+					response302Header(dos);
+				}
+				
 				
 				DataOutputStream dos = new DataOutputStream(out);
 				response302Header(dos);
+			}else if(url.endsWith(".css")) {
+				DataOutputStream dos = new DataOutputStream(out);
+				byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+				response200HeaderWithCss(dos, body.length);
+				responseBody(dos, body);
 			}
 			/*
 			 * 요구사항 1번 log.debug("request line : {}",line); if(line==null)return; String[]
@@ -79,12 +107,30 @@ public class RequestHandler extends Thread {
 			log.error(e.getMessage());
 		}
 	}
-
+	private void response200HeaderWithCss(DataOutputStream dos, int lengthOfBodyContent) {
+		try {
+			dos.writeBytes("HTTP/1.1 200 OK \r\n");
+			dos.writeBytes("Content-Type: text/css;charset=utf-8\r\n");
+			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
 	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
 		try {
 			dos.writeBytes("HTTP/1.1 200 OK \r\n");
 			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
 			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}private void response302HeaderWithCookie(DataOutputStream dos,String cookie) {
+		try {
+			dos.writeBytes("HTTP/1.1 302 Found \r\n");
+			dos.writeBytes("Location: /index.html\r\n");
+			dos.writeBytes("Set-Cookie: "+cookie + "\r\n");
 			dos.writeBytes("\r\n");
 		} catch (IOException e) {
 			log.error(e.getMessage());
